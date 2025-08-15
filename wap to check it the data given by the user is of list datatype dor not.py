fa836ae60dd1905744b9f5da7c3a826200297f08
aditya raj gupta <adityaraaj62@gmail.com>
2025-08-15 23:20:12 +0530

    13 AUG 2025 RACHNA MAAM

---
+++ wap to check it the data given by the user is of list datatype dor not.py
@@ -0,0 +1,6 @@
+#wap to check it the data given by the user is of list datatype dor not
+
+a=eval(input("Enter a collections:-"))
+
+if type(a)==list:
+    print(a, ",The give data is list datatype")
